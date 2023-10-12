d53a9773e7b76634e7e3d6bee44000a143678295
mubi715 <110280982+mubi715@users.noreply.github.com>
2023-10-12 13:28:03 +0000

    cintainer info done

---
+++ practice/dataConversion.js
@@ -0,0 +1,21 @@
+let number= 34
+let number1= String(number)
+console.log(number1);
+console.log(typeof number1);
+let x=100
+let y=++x
+console.log(y);
+let name=["ali","mubasher"]
+console.log(name);
+let object ={
+    name:"Maarij",
+    age:34
+
+}
+console.log(object);
+
+let myfunction = function name(params) {
+    console.log("hello world");
+    
+}
+console.log( typeof myfunction);
